211bda5b8d4b55664ae46057de4cfebb6a8d25c5
KusicFack <129599166+KusicFack@users.noreply.github.com>
2025-11-16 00:44:09 +0800

    Hifini resurrected

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 
 from sign_units.wuai import wuai_sign
 from sign_units.rousi import rousi_sign
+from sign_units.hifini import hifini_sign
 from sign_units.pcbeta import pcbeta_sign
 from sign_units.ablesci import ablesci_sign
 
@@ -27,9 +28,10 @@ if __name__ == "__main__":
     browser = webdriver.ChromiumDriver(options=chromium_options, service=chromium_service)
     blank_window = browser.current_window_handle
     
-    rank, total = 0, 4
+    rank, total = 0, 5
     rank = wuai_sign(browser, "cookies/wuai_cookie.json", blank_window, rank, total)
     rank = rousi_sign(browser, "cookies/rouzi_cookie.json", blank_window, rank, total)
+    rank = hifini_sign(browser, "cookies/hifini_cookie.json", blank_window, rank, total)
     rank = pcbeta_sign(browser, "cookies/pcbeta_cookie.json", blank_window, rank, total)
     rank = ablesci_sign(browser, "cookies/ablesci_cookie.json", blank_window, rank, total)
     browser.quit()
